ae8d6f1bea8406573587b3f8cd0eda185f620f1b
permaficus <abukhalif2019@gmail.com>
2024-04-30 21:47:43 +0700

    chore(controller): add update function

--- backend/src/v1/controller/index.ts
+++ backend/src/v1/controller/index.ts
@@ -1,5 +1,5 @@
 import { Model } from "../../model";
-import { Request, Response, NextFunction } from 'express'
+import { Request, Response, NextFunction, response } from 'express'
 
 export const storeContact = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -18,11 +18,11 @@ export const storeContact = async (req: Request, res: Response, next: NextFuncti
 }
 export const updateContact = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        await Model.update(req.body);
+        const response = await Model.update(req.body);
         res.status(200).json({
             status: 'OK',
             code: 200,
-            details: `updating ${req.body.length} contacts`
+            details: response
         })
     } catch (error: any) {
         res.status(500);
