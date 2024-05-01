82271d71fc0f57990e174558c722203a589da8c7
permaficus <abukhalif2019@gmail.com>
2024-05-01 14:07:55 +0700

    chore: add populate contacts function

--- backend/src/v1/routes/index.ts
+++ backend/src/v1/routes/index.ts
@@ -2,10 +2,11 @@ import Router from 'express'
 
 import { PathNotFound, errHandler } from '../middlewares/errHandler';
 import { validateRequest } from '../middlewares/requestValidation';
-import { storeContact, updateContact } from '../controller';
+import { populateContacts, storeContact, updateContact } from '../controller';
 
 export const router = Router();
 router.post('/store', validateRequest, storeContact);
 router.patch('/update', updateContact);
+router.get('/contacts', populateContacts)
 router.use(PathNotFound)
 router.use(errHandler)
